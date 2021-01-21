aa8d181c7efbbf572275c5ee3bde030946886d26
charliejhadley <charliejhadley@gmail.com>
2021-01-21 17:13:08 +0000

    store_prefs

---
+++ R/store_prefs.R
@@ -0,0 +1,14 @@
+#' Store current preferences
+#' @export
+store_prefs <- function(preferences_name = "personal") {
+  if (.Platform$OS.type == "unix") {
+    rstudio_prefs_json <- jsonlite::read_json("~/.config/rstudio/rstudio-prefs.json",
+                                              simplifyVector = TRUE)
+  } else {
+    stop("This package hasn't been tested on Windows")
+  }
+
+  dir.create(file.path(rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE)), showWarnings = FALSE)
+
+  jsonlite::write_json(rstudio_prefs_json, file.path(rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE), paste0(preferences_name, "_rstudio-prefs.json")))
+}
