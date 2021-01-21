6db50f614ea804423128c5fdd207a85151f90ab1
charliejhadley <charliejhadley@gmail.com>
2021-01-21 17:13:25 +0000

    apply_setting

--- R/store_prefs.R
+++ R/store_prefs.R
@@ -12,3 +12,19 @@ store_prefs <- function(preferences_name = "personal") {
 
   jsonlite::write_json(rstudio_prefs_json, file.path(rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE), paste0(preferences_name, "_rstudio-prefs.json")))
 }
+
+#' Apply setting
+#'
+apply_setting <- function(settings_json, setting_name){
+
+  pref_value <- unlist(settings_json[setting_name],
+                       recursive = FALSE)
+
+  if(length(pref_value) > 1){
+    names(pref_value) <- gsub("panes.", "", names(pref_value))
+    # return(pref_value)
+    rstudioapi::writeRStudioPreference(setting_name, pref_value)
+  } else
+    # unlist(pref_value, use.names = FALSE)
+    rstudioapi::writeRStudioPreference(setting_name, unlist(pref_value, use.names = FALSE))
+
