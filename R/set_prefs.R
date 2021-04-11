cd103541989f56e4c1d0133b99d81f907037933a
charliejhadley <charliejhadley@gmail.com>
2021-04-11 15:15:36 +0100

    styler

--- R/set_prefs.R
+++ R/set_prefs.R
@@ -1,14 +1,12 @@
 #' Set stored preferences
 #' @export
 prefs_set <- function(preferences_name = "personal") {
-
   new_settings <- jsonlite::read_json(file.path(rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE), paste0(preferences_name, "_rstudio-prefs.json")))
 
   prefs_write(new_settings)
 }
 
-prefs_write <- function(prefs){
-
+prefs_write <- function(prefs) {
   existing_settings <- jsonlite::read_json(usethis:::rstudio_config_path("rstudio-prefs.json"))
 
   for (name in names(prefs)) {
@@ -26,7 +24,6 @@ prefs_write <- function(prefs){
   jsonlite::write_json(updated_settings, usethis:::rstudio_config_path("rstudio-prefs.json"), auto_unbox = TRUE, pretty = TRUE)
 
   usethis:::restart_rstudio("A restart of RStudio is required to reload preferences")
-
 }
 
 #' Restore default preferences
@@ -36,21 +33,12 @@ prefs_write <- function(prefs){
 #' prefs_reset_to_defaults().
 #'
 #' @export
-prefs_reset_to_defaults <- function(improve_code_reproducibility = TRUE){
-
-  if(usethis::ui_yeah("This will delete your existing preferences file. RStudio will then automatically re-write this file. Are you okay with that?")){
-
-    if(usethis::ui_yeah("The default RStudio preferences include loading .RData and asking you about it all of the time. You are strongly recommended to run `prefs_improve_reproducibility()` after restarting RStudio - which you'll be prompted to do next")){
+prefs_reset_to_defaults <- function(improve_code_reproducibility = TRUE) {
+  if (usethis::ui_yeah("This will delete your existing preferences file. RStudio will then automatically re-write this file. Are you okay with that?")) {
+    if (usethis::ui_yeah("The default RStudio preferences include loading .RData and asking you about it all of the time. You are strongly recommended to run `prefs_improve_reproducibility()` after restarting RStudio - which you'll be prompted to do next")) {
       file.remove(usethis:::rstudio_config_path("rstudio-prefs.json"))
 
       usethis:::restart_rstudio("A restart of RStudio is required to reload the default preferences")
     }
-
   }
 }
-
-
-
-
-
-
