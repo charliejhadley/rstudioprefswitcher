c32739952bc48a172194a3da0365b1838e923213
charliejhadley <charliejhadley@gmail.com>
2021-04-11 15:14:20 +0100

    remove windows check

--- R/store_prefs.R
+++ R/store_prefs.R
@@ -1,15 +1,11 @@
 #' Store current preferences
 #' @export
-store_prefs <- function(preferences_name = "personal") {
-  if (.Platform$OS.type == "unix") {
 prefs_save <- function(preferences_name = "personal") {
-    rstudio_prefs_json <- jsonlite::read_json("~/.config/rstudio/rstudio-prefs.json",
-                                              simplifyVector = TRUE)
-  } else {
-    stop("This package hasn't been tested on Windows")
-  }
 
-  dir.create(file.path(rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE)), showWarnings = FALSE)
+    rstudio_prefs_json <- jsonlite::read_json(usethis:::rstudio_config_path("rstudio-prefs.json"),
+                                              simplifyVector = TRUE)
+
+  dir.create(file.path(rappdirs::user_config_dir("rstudioprefswitcher")), recursive = TRUE, showWarnings = FALSE)
 
   jsonlite::write_json(rstudio_prefs_json, file.path(rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE),
                                                      paste0(preferences_name, "_rstudio-prefs.json")),
