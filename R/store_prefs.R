acf15fbc2ee520ec85371f7b83523be2b571946c
charliejhadley <charliejhadley@gmail.com>
2021-04-11 15:14:41 +0100

    styler

--- R/store_prefs.R
+++ R/store_prefs.R
@@ -1,13 +1,23 @@
 #' Store current preferences
 #' @export
 prefs_save <- function(preferences_name = "personal") {
+  rstudio_prefs_json <-
+    jsonlite::read_json(usethis:::rstudio_config_path("rstudio-prefs.json"),
+                        simplifyVector = TRUE)
 
-    rstudio_prefs_json <- jsonlite::read_json(usethis:::rstudio_config_path("rstudio-prefs.json"),
-                                              simplifyVector = TRUE)
+  dir.create(
+    file.path(rappdirs::user_config_dir("rstudioprefswitcher")),
+    recursive = TRUE,
+    showWarnings = FALSE
+  )
 
-  dir.create(file.path(rappdirs::user_config_dir("rstudioprefswitcher")), recursive = TRUE, showWarnings = FALSE)
-
-  jsonlite::write_json(rstudio_prefs_json, file.path(rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE),
-                                                     paste0(preferences_name, "_rstudio-prefs.json")),
-                       auto_unbox = TRUE, pretty = TRUE)
+  jsonlite::write_json(
+    rstudio_prefs_json,
+    file.path(
+      rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE),
+      paste0(preferences_name, "_rstudio-prefs.json")
+    ),
+    auto_unbox = TRUE,
+    pretty = TRUE
+  )
 }
