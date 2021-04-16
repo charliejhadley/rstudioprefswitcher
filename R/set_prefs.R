ee10a3f3cbc2665e1a15be2270cff0a9c3af1239
charliejhadley <charliejhadley@gmail.com>
2021-04-16 10:38:06 +0100

    Fix https://github.com/charliejhadley/rstudioprefswitcher/issues/1
    lowercase filepaths for linux

--- R/set_prefs.R
+++ R/set_prefs.R
@@ -1,13 +1,13 @@
 #' Set stored preferences
 #' @export
 prefs_set <- function(preferences_name = "personal") {
-  new_settings <- jsonlite::read_json(file.path(rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE), paste0(preferences_name, "_rstudio-prefs.json")))
+  new_settings <- jsonlite::read_json(tolower(file.path(rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE), paste0(preferences_name, "_rstudio-prefs.json"))))
 
   prefs_write(new_settings)
 }
 
 prefs_write <- function(prefs) {
-  existing_settings <- jsonlite::read_json(usethis:::rstudio_config_path("rstudio-prefs.json"))
+  existing_settings <- jsonlite::read_json(tolower(usethis:::rstudio_config_path("rstudio-prefs.json")))
 
   for (name in names(prefs)) {
     val <- prefs[[name]]
@@ -21,7 +21,7 @@ prefs_write <- function(prefs) {
 
   updated_settings <- existing_settings
 
-  jsonlite::write_json(updated_settings, usethis:::rstudio_config_path("rstudio-prefs.json"), auto_unbox = TRUE, pretty = TRUE)
+  jsonlite::write_json(updated_settings, tolower(usethis:::rstudio_config_path("rstudio-prefs.json")), auto_unbox = TRUE, pretty = TRUE)
 
   usethis:::restart_rstudio("A restart of RStudio is required to reload preferences")
 }
@@ -36,7 +36,7 @@ prefs_write <- function(prefs) {
 prefs_reset_to_defaults <- function(improve_code_reproducibility = TRUE) {
   if (usethis::ui_yeah("This will delete your existing preferences file. RStudio will then automatically re-write this file. Are you okay with that?")) {
     if (usethis::ui_yeah("The default RStudio preferences include loading .RData and asking you about it all of the time. You are strongly recommended to run `prefs_improve_reproducibility()` after restarting RStudio - which you'll be prompted to do next")) {
-      file.remove(usethis:::rstudio_config_path("rstudio-prefs.json"))
+      file.remove(tolower(usethis:::rstudio_config_path("rstudio-prefs.json")))
 
       usethis:::restart_rstudio("A restart of RStudio is required to reload the default preferences")
     }
--- R/store_prefs.R
+++ R/store_prefs.R
@@ -1,12 +1,13 @@
 #' Store current preferences
 #' @export
 prefs_save <- function(preferences_name = "personal") {
+
   rstudio_prefs_json <-
-    jsonlite::read_json(usethis:::rstudio_config_path("rstudio-prefs.json"),
+    jsonlite::read_json(tolower(usethis:::rstudio_config_path("rstudio-prefs.json")),
                         simplifyVector = TRUE)
 
   dir.create(
-    file.path(rappdirs::user_config_dir("rstudioprefswitcher")),
+    file.path(tolower(rappdirs::user_config_dir("rstudioprefswitcher"))),
     recursive = TRUE,
     showWarnings = FALSE
   )
@@ -14,7 +15,7 @@ prefs_save <- function(preferences_name = "personal") {
   jsonlite::write_json(
     rstudio_prefs_json,
     file.path(
-      rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE),
+      tolower(rappdirs::user_config_dir("rstudioprefswitcher", expand = TRUE)),
       paste0(preferences_name, "_rstudio-prefs.json")
     ),
     auto_unbox = TRUE,
